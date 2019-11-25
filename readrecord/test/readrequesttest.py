17e81c358d95a84fcdc18e57b2b2a06ad87cd641
liangman <liangman@obook.com.cn>
2019-11-25 16:29:17 +0800

    重命名：   test/readrequest.py -> test/readrequesttest.py

--- readrecord/test/readrequesttest.py
+++ readrecord/test/readrequesttest.py
@@ -14,10 +14,19 @@ class ReadRequest:
         print response.read()
 
 
+    def getPeriodReadWordTime(self):
+        request = urllib2.Request('http://127.0.0.1:8000/readrecord/readrecordapi.aspx/')
+        request.add_header('action', 'getPeriodReadWordTime')
+        request.add_header('serial', 'OF6IC31811B00030')
+        response = urllib2.urlopen(request)
+
+        print response.read()
+
 
 
 
 if __name__ == '__main__':
     testObject = ReadRequest()
 
-    testObject.getEveryBookReadData()
+    # testObject.getEveryBookReadData()
+    testObject.getPeriodReadWordTime()
